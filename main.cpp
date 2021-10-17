e78aaacbad571003015ebe53b199082a52843bf9
Ayush Singh <ayushsnh711@gmail.com>
2021-10-17 15:01:28 +0530

    Added code template to the main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <bits/stdc++.h>
 
-#define int 			long long
-#define pb				push_back
-#define vi				vector<int>
-#define w(x)			int x; cin >> x; while (x--)
+#define ll	  long long
+#define pb push_back
+#define vi  vector<int>
+#define w(x)int x; cin >> x; while (x--)
+#define ld long double
+#define pb push_back
+#define pll pair<ll,ll>
+#define all(a) (a).begin(),(a).end()
+#define F first
+#define S second
+#define oset ordered_set
+#define rep(i,j,k) for(ll i=j;i<k;i++)
+#define endl "\n"
+
 
 int main(void) {
   
