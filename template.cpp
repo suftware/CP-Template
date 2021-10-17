3ffec13c94c2b51ad420eed4135ccc474dc42c40
Ayush Singh <ayushsnh711@gmail.com>
2021-10-17 07:46:06 +0530

    Added a template of my CP code

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+#define pll pair<ll,ll>
+#define all(a) (a).begin(),(a).end()
+#define F first
+#define S second
+#define oset ordered_set
+#define rep(i,j,k) for(ll i=j;i<k;i++)
+#define endl "\n"
+
+void solve()
+{
+  //Add Code
+}
+int main() {
+  int testcases;
+  cin>>testcases;
+  while(testcases--)
+  {solve();}
+}
